aa8fdb26fe8e68268c9bbf9f38d5894bfa6acfef
Campbell Moss <campbell.moss@fiserv.com>
2025-03-25 15:02:05 +1300

    Disable access control POC

--- src/collections/access/cardTenants.ts
+++ src/collections/access/cardTenants.ts
@@ -3,7 +3,7 @@ import type { Access } from 'payload'
 export const cardTenants: Access = ({ req: { user } }) => {
   // TODO: Replace with actual tenant check
   if (user) {
-    return user.email === 'campbell.moss@gmail.com'
+    return true
   }
 
   return false
